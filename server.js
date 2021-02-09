8cb8cab973db8358ad48e02905a30ebe0df4fb2c
DPK <dougkvamme@gmail.com>
2021-02-08 18:36:21 -0600

    updating server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 // Setting up Express App
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Requires the .dotenv file
 require("dotenv").config();
 
 // db mongo
